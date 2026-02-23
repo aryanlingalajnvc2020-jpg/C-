d6e8163409dc8fb47373833f7090a9d755d31a58
Aryan Lingala <aryanlingalajnvc2020@gmail.com>
2026-02-23 10:15:34 +0530

    Create swapValues() with integer, oat, and string types.cpp

---
+++ swapValues() with integer, oat, and string types.cpp
@@ -0,0 +1,40 @@
+#include<iostream>
+using namespace std;
+class SWAP{
+    int i;float j;char k;
+    public:
+    void swap(int a,int b){
+        i=a;
+        a=b;
+        b=i;
+        cout<<"after swap"<<endl<<"a="<<a<<endl<<"b="<<b<<endl;
+    }
+    void swap(float c,float d){
+        j=c;
+        c=d;
+        d=j;
+        cout<<"after swap"<<endl<<"c="<<c<<endl<<"d="<<d<<endl;
+    }
+    void swap(char e,char f){
+        k=e;
+        e=f;
+        f=k;
+        cout<<"after swap"<<endl<<"e="<<e<<endl<<"f="<<f<<endl;
+    }
+};
+int main(){
+    SWAP s;
+    s.swap(10,15);
+    s.swap(10.5f,11.5f);
+    s.swap('A','B');
+}
+output:
+after swap
+a=15
+b=10
+after swap
+c=11.5
+d=10.5
+after swap
+e=B
+f=A
